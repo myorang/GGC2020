49698497d81fded394517c4c072b6eea052acbc4
ekjh22 <38350812+ekjh22@users.noreply.github.com>
2019-10-30 10:37:00 +0900

    ffff2

---
+++ GGC Unity Project/Assets/Scripts/InGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGame : MonoBehaviour
+{
+    public BulletManager bulletManager;
+    public bool isMusicStart = false;
+    public Slider slider;
+
+    [Range(0f, 1f)]
+    public float value;
+
+    void Start()
+    {
+        StartCoroutine(GameStart());
+    }
+
+    public IEnumerator GameStart()
+    {
+        yield return new WaitForSeconds(1f);
+        
+        isMusicStart = true;
+
+        //EffectMgr.SpawnSprite("Particle 1", transform, 3);
+        SoundMgr.Instance.PlayLoopSound("phase3", "BGM");
+        bulletManager.MusicStart();
+    }
+
+    void Update()
+    {
+        //EffectMgr.SpawnSprite("Particle 1", transform, 1);
+        EffectMgr.SliderValue(slider, value);
+    }
+}
